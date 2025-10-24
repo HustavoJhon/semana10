598be9ddbe1bab250933bda291c1fa3d16858f83
Cebitaz <EstudianteUC@UCUXA60228.continental.edu.pe>
2025-10-24 16:54:44 -0500

    cambio total

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
-const dark = document.getElementById("toogle");
-const link  =  document.querySelector('link[rel="stylesheet"]');
+const toggle = document.getElementById("toogle");
+const link = document.querySelector('link[rel="stylesheet"]');
 
 toggle.addEventListener("click", () => {
-    if (link.getAttribute("href") === "src/css/style.css") {
-      link.setAttribute("href", "src/css/dark.css");
-      dark.textContent = "☀️ Modo Claro";
-    } else {
-      link.setAttribute("href", "src/css/style.css");
-      dark.textContent = "🌙 Modo Oscuro";
-    }
-  });
+  if (link.getAttribute("href") === "src/css/style.css") {
+    link.setAttribute("href", "src/css/darkmode.css");
+    toggle.textContent = "☀️ Modo Claro";
+  } else {
+    link.setAttribute("href", "src/css/style.css");
+    toggle.textContent = "🌙 Modo Oscuro";
+  }
+});
